cdb69648dd38552f582ed79d2eb98b3ff4b7dc7a
ivyycore <mariasequeira.dev@gmail.com>
2025-04-10 23:54:16 +0100

    Created Cost in Unit

--- GameUnit/Unit.cs
+++ GameUnit/Unit.cs
@@ -7,13 +7,14 @@ namespace GameUnit
     {
         private readonly int movement;
         public int Health { get; set; }
+        public abstract float Cost { get; }
 
         public Unit(int movement, int health)
         {
             this.movement = movement;
             Health = health;
         }
-        
+
         public void Move(int movement)
         {
             Console.WriteLine($"This Unit moved {movement.ToRoman()} houses");
