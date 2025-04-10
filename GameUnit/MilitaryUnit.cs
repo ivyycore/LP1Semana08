c178aa9a3d9eb9a27f3c96d1c2a70d7350ba2605
ivyycore <mariasequeira.dev@gmail.com>
2025-04-10 23:57:19 +0100

    Changed Health and Cost in MilitaryUnit

--- GameUnit/MilitaryUnit.cs
+++ GameUnit/MilitaryUnit.cs
@@ -1,14 +1,35 @@
 namespace GameUnit
 {
-    public class MilitaryUnit : Unit // Heranca!!
+    public class MilitaryUnit : Unit // Heranca
     {
         public int AttackPower { get; }
         public int XP { get; private set; }
+        public override int Health { get => base.Health+XP; }
+
 
         public MilitaryUnit(int mov, int health, int attackPower) : base(mov, health)
         {                                                        // Unit(int, int)
             AttackPower = attackPower;
             XP = 0;
+        }
+
+        public override float Cost
+        {
+            get 
+            {
+                return AttackPower+XP;
+            }
+        }
+
+        public void Attack(Unit u)
+        {
+            XP++;
+            u.Health -= AttackPower;
+        }
+
+        public override string ToString()
+        {
+            return $"SettlerUnit: HP={Health} Cost={Cost:f2} AP={AttackPower} XP={XP}";
         }
     }
 }
